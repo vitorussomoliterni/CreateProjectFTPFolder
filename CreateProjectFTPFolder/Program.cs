d48c83d169277c45c19fed9473c8d47db36fdbc5
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-08-12 17:57:21 +1000

    Written method to check user input.

--- CreateProjectFTPFolder/Program.cs
+++ CreateProjectFTPFolder/Program.cs
@@ -15,10 +15,35 @@ namespace CreateProjectFTPFolder
 
         private static void ShowMenu()
         {
+            Console.WriteLine("This program will create a template folder for a new project in the FTP drive.\n");
+
             while (true)
             {
+                Console.Write("Enter the project number (type 'exit' to exit the program): ");
+                var userInput = Console.ReadLine();
+                var projectNumber = userInput.Trim().ToLower();
 
+                if (projectNumber.Equals("exit"))
+                {
+                    break;
+                }
+
+                if (!CheckInput(projectNumber))
+                {
+                    Console.WriteLine("Invalid project number. The project number must consist in 6 digits.\n");
+                }
             }
+        }
+
+        private static bool CheckInput(string projectNumber)
+        {
+            int integer;
+            if (projectNumber.Length != 6 || !int.TryParse(projectNumber, out integer))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
